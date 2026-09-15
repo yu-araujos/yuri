d70f6e11d1839732cfbc45ecd47e76c717bca3aa
Yuri Silva <yu.dev@icloud.com>
2026-09-16 00:38:10 +0100

    refactor: remove unused dependencies and utility types

--- src/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { CardProps } from "./card";
-export type { Contact } from "./contact";
-export type { Project } from "./project";
-export type { ContributionDay } from "./github";
-export type { SidebarProps } from "./sidebar";
